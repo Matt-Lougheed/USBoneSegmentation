f81ddc9c1a54e1b21c1cc0e1f1901a9c7af2d1f0
Matt Lougheed <lougheed@cs.queensu.ca>
2016-06-14 16:32:12 -0400

    Added USGeometry module which computes metrics based on scanlines in linear and curvilinear geometries

--- USGeometry/USGeometry.py
+++ USGeometry/USGeometry.py
@@ -3,6 +3,7 @@ import unittest
 import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
 import logging
+import numpy, math
 
 #
 # USGeometry
@@ -61,7 +62,7 @@ class USGeometryWidget(ScriptedLoadableModuleWidget):
     self.inputSelector.showChildNodeTypes = False
     self.inputSelector.setMRMLScene( slicer.mrmlScene )
     self.inputSelector.setToolTip( "Pick the input volume for geometry viewing." )
-    inputsFormLayout.addRow("Input Volume: ", self.inputSelector)
+    inputsFormLayout.addRow("Input volume: ", self.inputSelector)
 
     #
     # Configuration file selector
@@ -88,10 +89,25 @@ class USGeometryWidget(ScriptedLoadableModuleWidget):
     inputsFormLayout.addRow("Manual segmentations directory: ", directoryLayout)
 
     #
-    # OUTPUTS Area
+    # Input volume selector
+    #
+    self.algorithmSegmentationSelector = slicer.qMRMLNodeComboBox()
+    self.algorithmSegmentationSelector.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
+    self.algorithmSegmentationSelector.selectNodeUponCreation = True
+    self.algorithmSegmentationSelector.addEnabled = False
+    self.algorithmSegmentationSelector.removeEnabled = False
+    self.algorithmSegmentationSelector.noneEnabled = False
+    self.algorithmSegmentationSelector.showHidden = False
+    self.algorithmSegmentationSelector.showChildNodeTypes = False
+    self.algorithmSegmentationSelector.setMRMLScene( slicer.mrmlScene )
+    self.algorithmSegmentationSelector.setToolTip( "Pick the algorithm segmentation to evaluate." )
+    inputsFormLayout.addRow("Algorithm segmentation: ", self.algorithmSegmentationSelector)
+
+    #
+    # OUTPUT VOLUMES Area
     #
     outputsCollapsibleButton = ctk.ctkCollapsibleButton()
-    outputsCollapsibleButton.text = "Outputs"
+    outputsCollapsibleButton.text = "Output Volumes"
     self.layout.addWidget(outputsCollapsibleButton)
     outputsFormLayout = qt.QFormLayout(outputsCollapsibleButton)
 
@@ -102,13 +118,14 @@ class USGeometryWidget(ScriptedLoadableModuleWidget):
     self.mergedManualSegmentations.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
     self.mergedManualSegmentations.selectNodeUponCreation = True
     self.mergedManualSegmentations.addEnabled = True
-    self.mergedManualSegmentations.removeEnabled = False
+    self.mergedManualSegmentations.renameEnabled = True
+    self.mergedManualSegmentations.removeEnabled = True
     self.mergedManualSegmentations.noneEnabled = False
     self.mergedManualSegmentations.showHidden = False
     self.mergedManualSegmentations.showChildNodeTypes = False
     self.mergedManualSegmentations.setMRMLScene( slicer.mrmlScene )
     self.mergedManualSegmentations.setToolTip( "Pick the label map for the merged manual segmentations." )
-    outputsFormLayout.addRow("Merged Manual Segmentations: ", self.mergedManualSegmentations)
+    outputsFormLayout.addRow("Merged manual segmentations: ", self.mergedManualSegmentations)
 
     #
     # Scanline label map
@@ -117,7 +134,8 @@ class USGeometryWidget(ScriptedLoadableModuleWidget):
     self.scanlines.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
     self.scanlines.selectNodeUponCreation = True
     self.scanlines.addEnabled = True
-    self.scanlines.removeEnabled = False
+    self.scanlines.renameEnabled = True
+    self.scanlines.removeEnabled = True
     self.scanlines.noneEnabled = False
     self.scanlines.showHidden = False
     self.scanlines.showChildNodeTypes = False
@@ -126,12 +144,50 @@ class USGeometryWidget(ScriptedLoadableModuleWidget):
     outputsFormLayout.addRow("Scanlines: ", self.scanlines)
 
     #
+    # Scanline label map
+    #
+    self.outputSegmentation = slicer.qMRMLNodeComboBox()
+    self.outputSegmentation.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
+    self.outputSegmentation.selectNodeUponCreation = True
+    self.outputSegmentation.addEnabled = True
+    self.outputSegmentation.renameEnabled = True
+    self.outputSegmentation.removeEnabled = True
+    self.outputSegmentation.noneEnabled = False
+    self.outputSegmentation.showHidden = False
+    self.outputSegmentation.showChildNodeTypes = False
+    self.outputSegmentation.setMRMLScene( slicer.mrmlScene )
+    self.outputSegmentation.setToolTip( "Pick the label map for the output segmentation." )
+    outputsFormLayout.addRow("Output segmentation: ", self.outputSegmentation)
+
+    #
+    # OUTPUT METRIC Area
+    #
+    outputMetricsCollapsibleButton = ctk.ctkCollapsibleButton()
+    outputMetricsCollapsibleButton.text = "Output Metrics"
+    self.layout.addWidget(outputMetricsCollapsibleButton)
+    outputMetricsFormLayout = qt.QFormLayout(outputMetricsCollapsibleButton)
+
+    #
+    # False negative metric
+    #
+    self.falseNegativeMetric = qt.QLineEdit()
+    self.falseNegativeMetric.setReadOnly(True)
+    outputMetricsFormLayout.addRow("False negative percentage: ", self.falseNegativeMetric)
+
+    #
+    # False negative metric
+    #
+    self.truePositiveMetric = qt.QLineEdit()
+    self.truePositiveMetric.setReadOnly(True)
+    outputMetricsFormLayout.addRow("True positive percentage: ", self.truePositiveMetric)
+
+    #
     # Apply Button
     #
     self.applyButton = qt.QPushButton("Apply")
     self.applyButton.toolTip = "Run the algorithm."
     self.applyButton.enabled = False
-    outputsFormLayout.addRow(self.applyButton)
+    outputMetricsFormLayout.addRow(self.applyButton)
 
     # Connections
     self.applyButton.connect('clicked(bool)', self.onApplyButton)
@@ -163,7 +219,7 @@ class USGeometryWidget(ScriptedLoadableModuleWidget):
 
   def onApplyButton(self):
     logic = USGeometryLogic()
-    logic.run(self.inputSelector.currentNode(), self.configFile.text, self.directory.text, self.mergedManualSegmentations.currentNode(), self.scanlines.currentNode())
+    logic.run(self.inputSelector.currentNode(), self.configFile.text, self.directory.text, self.mergedManualSegmentations.currentNode(), self.scanlines.currentNode(), self.outputSegmentation.currentNode())
 
 #
 # USGeometryLogic
@@ -179,27 +235,7 @@ class USGeometryLogic(ScriptedLoadableModuleLogic):
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
 
-
-  def scanlineEndPoints(self, scanlineIndex):
-    import math
-
-    # Compute the angle of the scanline
-    angle = thetaStartDeg + scanlineIndex*degreesPerScanline
-    angleRadians = math.pi * angle / 180
-
-    # Compute the starting point
-    startScanlineX = circleCenter[0] + math.sin(angleRadians) * radiusStartMm / outputImageSpacing[0]
-    startScanlineY = circleCenter[1] + math.cos(angleRadians) * radiusStartMm / outputImageSpacing[1]
-    startScanline = [startScanlineX, startScanlineY]
-
-    # Compute the ending point
-    endScanLineX = circleCenter[0] + math.sin(angleRadians) * radiusStopMm / outputImageSpacing[0]
-    endScanLineY = circleCenter[1] + math.cos(angleRadians) * radiusStopMm / outputImageSpacing[1]
-    endScanline = [endScanLineX, endScanLineY]
-
-    return [startScanline, endScanline]
-
-  def run(self, inputVolume, configFileName, manualSegmentationsDirectory, mergedVolume, scanlineVolume):
+  def run(self, inputVolume, configFileName, manualSegmentationsDirectory, mergedVolume, scanlineVolume, outputSegmentationVolume):
     """
     Run the actual algorithm
     """
@@ -216,6 +252,12 @@ class USGeometryLogic(ScriptedLoadableModuleLogic):
     summedImage.SetExtent(reader.GetOutput().GetExtent())
     summedImage.AllocateScalars(vtk.VTK_UNSIGNED_CHAR,1)
     summedImage.ShallowCopy(reader.GetOutput())
+
+    # Image data for output segmentation
+    outputSegmentation = vtk.vtkImageData()
+    outputSegmentation.SetExtent(reader.GetOutput().GetExtent())
+    outputSegmentation.AllocateScalars(vtk.VTK_UNSIGNED_CHAR,1)
+
 
     # Initialize filter to add images together
     mathFilter = vtk.vtkImageMathematics()
@@ -249,8 +291,99 @@ class USGeometryLogic(ScriptedLoadableModuleLogic):
     inputExtent = inputImageData.GetExtent()
 
     # Parse input config file
+    US_Geometry = UltrasoundTransducerGeometry(configFileName)
+
+    # Setup the canvas filter to draw scanlines
+    imgDim = inputVolume.GetImageData().GetDimensions()
+    drawFilter = vtk.vtkImageCanvasSource2D()
+    drawFilter.SetExtent(0,imgDim[0]-1,0,imgDim[1]-1,0,0)
+    drawFilter.SetDrawColor(1)
+
+    # Points for the output segmentation
+    outputSegmentationPoints = vtk.vtkPoints()
+
+    # Iterate over scanlines and draw line
+    for i in range(US_Geometry.numberOfScanlines):
+      # Compute scanline endpoints
+      [startScanline, endScanline] = US_Geometry.scanlineEndPoints(i)
+      print("Scanline start: [{} {}".format(startScanline[0],startScanline[1]))
+      print("Scanline end: [{} {}".format(endScanline[0],endScanline[1]))
+
+      # Draw the scanline
+      drawFilter.FillTube(int(startScanline[0]),int(startScanline[1]),int(endScanline[0]),int(endScanline[1]),1)
+
+      # Compute the manual segmentation points on each scanline
+      for z in range(imgDim[2]):
+        print("Creating line for slice {}".format(z))
+        currentLine = vtk.vtkLineSource()
+        currentStartPoint = [startScanline[0], startScanline[1], z]
+        currentEndPoint = [endScanline[0], endScanline[1], z]
+        currentLine.SetPoint1(currentStartPoint)
+        currentLine.SetPoint2(currentEndPoint)
+        currentLine.SetResolution(US_Geometry.numberOfSamplesPerScanline)
+        currentLine.Update()
+
+        # Get the points on the line
+        denom = math.sqrt((currentEndPoint[0] - currentStartPoint[0])**2 + (currentEndPoint[1] - currentStartPoint[1])**2)
+        unitVector = [(currentEndPoint[0] - currentStartPoint[0])/denom, (currentEndPoint[1] - currentStartPoint[1])/denom]
+        linePoints = currentLine.GetOutput()
+        xVals = []
+        yVals = []
+        for j in range(linePoints.GetNumberOfPoints()):
+          currentPoint = linePoints.GetPoint(j)
+          summedSegPoint = summedImage.GetScalarComponentAsDouble(int(currentPoint[0]),int(currentPoint[1]),int(currentPoint[2]),0)
+          if (summedSegPoint > 0):
+            xVals.extend([int(currentPoint[0]) for _ in range(int(summedSegPoint))]) # Add points current X value X number of times where X is overlap count
+            yVals.extend([int(currentPoint[1]) for _ in range(int(summedSegPoint))])
+            print("The point [{}, {}, {}] has value {}!".format(int(currentPoint[0]), int(currentPoint[1]), int(currentPoint[2]), summedSegPoint))
+
+        # If there are points on the line, compute mean / std
+        if (len(xVals) > 0):
+          # Compute mean point
+          xMean = sum(xVals)/len(xVals)
+          yMean = sum(yVals)/len(yVals)
+          outputSegmentation.SetScalarComponentFromDouble(xMean,yMean,z,0,255)
+
+          # Compute region edges from standard deviation
+          xStd = numpy.std(xVals)
+          yStd = numpy.std(yVals)
+          stdSum = xStd + yStd
+          toleranceFactor = 5 # What should be used as value here?
+          acceptableRegionPoint1 = [int(xMean + toleranceFactor*stdSum * unitVector[0]), int(yMean + toleranceFactor*stdSum * unitVector[1])]
+          acceptableRegionPoint2 = [int(xMean + -toleranceFactor*stdSum * unitVector[0]), int(yMean + -toleranceFactor*stdSum * unitVector[1])]          
+          outputSegmentation.SetScalarComponentFromDouble(acceptableRegionPoint1[0],acceptableRegionPoint1[1],z,0,100)
+          outputSegmentation.SetScalarComponentFromDouble(acceptableRegionPoint2[0],acceptableRegionPoint2[1],z,0,100)
+
+          outputSegmentationPoints.InsertNextPoint(xMean,yMean,z)
+      print("Finished scanline #{} processing".format(i))
+
+    # Draw scanlines
+    drawFilter.Update()
+
+    # Copy scanline slice to match the Z-dimension of input US volume
+    numOfSlices = imgDim[2]
+    imageAppendFilter = vtk.vtkImageAppend()
+    imageAppendFilter.SetAppendAxis(2)
+    for _ in range(numOfSlices):
+      imageAppendFilter.AddInputData(drawFilter.GetOutput())
+    imageAppendFilter.Update()
+
+    # Set scanline imagedata
+    scanlineVolume.SetIJKToRASMatrix(ijkToRas)
+    scanlineVolume.SetRASToIJKMatrix(rasToIjk)
+    scanlineVolume.SetAndObserveImageData(imageAppendFilter.GetOutput())
+
+    # Set output segmentation imagedata
+    outputSegmentationVolume.SetIJKToRASMatrix(ijkToRas)
+    outputSegmentationVolume.SetRASToIJKMatrix(rasToIjk)
+    outputSegmentationVolume.SetAndObserveImageData(outputSegmentation)
+
+    return True
+
+class UltrasoundTransducerGeometry:
+  def __init__(self, configFile):
     from xml.dom import minidom
-    parser = minidom.parse(configFileName)
+    parser = minidom.parse(configFile)
     scanConversionElement = parser.getElementsByTagName("ScanConversion")
     if (len(scanConversionElement) < 1):
       slicer.util.errorDisplay("Could not find ScanConversion element in configuration file!")
@@ -261,53 +394,70 @@ class USGeometryLogic(ScriptedLoadableModuleLogic):
 
     scanConversionElement = scanConversionElement[0]
 
-    # Get scan conversion attributes
-    global thetaStartDeg, thetaStopDeg, transducerCenterPixel, radiusStartMm, radiusStopMm, outputImageSpacing, circleCenter, degreesPerScanline
-    thetaStartDeg = float(scanConversionElement.attributes['ThetaStartDeg'].value)
-    thetaStopDeg = float(scanConversionElement.attributes['ThetaStopDeg'].value)
-    transducerCenterPixel = scanConversionElement.attributes['TransducerCenterPixel'].value
-    transducerCenterPixel = map(int,transducerCenterPixel.split(" "))
-    radiusStartMm = float(scanConversionElement.attributes['RadiusStartMm'].value)
-    radiusStopMm = float(scanConversionElement.attributes['RadiusStopMm'].value)
-    numberOfScanlines = int(scanConversionElement.attributes['NumberOfScanLines'].value)
-    outputImageSpacing = scanConversionElement.attributes['OutputImageSpacingMmPerPixel'].value
-    outputImageSpacing = map(float,outputImageSpacing.split(" "))
+    # Values common to both linear and curvilinear
+    self.transducerGeometry = scanConversionElement.attributes['TransducerGeometry'].value
+    self.transducerCenterPixel = scanConversionElement.attributes['TransducerCenterPixel'].value
+    self.transducerCenterPixel = map(int, self.transducerCenterPixel.split(" "))
+    self.numberOfScanlines = int(scanConversionElement.attributes['NumberOfScanLines'].value)
+    self.outputImageSpacing = scanConversionElement.attributes['OutputImageSpacingMmPerPixel'].value
+    self.outputImageSpacing = map(float, self.outputImageSpacing.split(" "))
+    self.numberOfSamplesPerScanline = int(scanConversionElement.attributes['NumberOfSamplesPerScanLine'].value)
 
-    # Find angle between scanlines
-    totalDeg = abs(thetaStopDeg - thetaStartDeg)
-    degreesPerScanline = totalDeg / numberOfScanlines
+    # Values just for curvilinear
+    if (self.transducerGeometry == "CURVILINEAR"):
+      self.thetaStartDeg = float(scanConversionElement.attributes['ThetaStartDeg'].value)
+      self.thetaStopDeg = float(scanConversionElement.attributes['ThetaStopDeg'].value)
+      self.radiusStartMm = float(scanConversionElement.attributes['RadiusStartMm'].value)
+      self.radiusStopMm = float(scanConversionElement.attributes['RadiusStopMm'].value)
+      self.totalDeg = abs(self.thetaStopDeg - self.thetaStartDeg)
+      self.degreesPerScanline = self.totalDeg / self.numberOfScanlines
+      self.circleCenter = [self.transducerCenterPixel[0], self.transducerCenterPixel[1] - self.radiusStartMm/self.outputImageSpacing[1]]
+    # Values just for linear
+    elif (self.transducerGeometry == "LINEAR"):
+      self.transducerWidthMm = float(scanConversionElement.attributes['TransducerWidthMm'].value)
+      self.imagingDepthMm = float(scanConversionElement.attributes['ImagingDepthMm'].value)
+      self.imageWidthPixel = self.transducerWidthMm / float(self.outputImageSpacing[0])
+      print("Image width pixel: {}".format(self.imageWidthPixel))
+      self.topLeftPixel = [self.transducerCenterPixel[0] - 0.5 * float(self.imageWidthPixel), self.transducerCenterPixel[1]]
+      print("Top left pixel: {}".format(self.topLeftPixel))
+      self.scanlineSpacingPixels = self.imageWidthPixel / float(self.numberOfScanlines)
+      self.scanlineLengthPixels = self.imagingDepthMm / self.outputImageSpacing[1]
+      print("Scanline length pixels: {}".format(self.scanlineLengthPixels))
 
-    # Circle center
-    circleCenter = [transducerCenterPixel[0], transducerCenterPixel[1] - radiusStartMm/outputImageSpacing[1]]
+  def scanlineEndPoints(self, scanline):
+    import math
+    # Compute curvilinear xy values
+    if (self.transducerGeometry == "CURVILINEAR"):
 
-    # Setup the canvas filter to draw scanlines
-    imgDim = inputVolume.GetImageData().GetDimensions()
-    drawFilter = vtk.vtkImageCanvasSource2D()
-    drawFilter.SetExtent(0,imgDim[0]-1,0,imgDim[1]-1,0,0)
-    drawFilter.SetDrawColor(1)
+      # Compute angle for desired scanline
+      angle = self.thetaStartDeg + scanline * self.degreesPerScanline
+      angleRadians = math.pi * angle / 180
 
-    # Iterate over scanlines and draw line
-    for i in range(numberOfScanlines):
-      # Compute scanline endpoints
-      [startScanline, endScanline] = self.scanlineEndPoints(i)
-      print("Scanline start: [{} {}".format(startScanline[0],startScanline[1]))
-      print("Scanline end: [{} {}".format(endScanline[0],endScanline[1]))
-      drawFilter.FillTube(int(startScanline[0]),int(startScanline[1]),int(endScanline[0]),int(endScanline[1]),1)
-    drawFilter.Update()
+      # Compute the starting point
+      startScanlineX = self.circleCenter[0] + math.sin(angleRadians) * self.radiusStartMm / self.outputImageSpacing[0]
+      startScanlineY = self.circleCenter[1] + math.cos(angleRadians) * self.radiusStartMm / self.outputImageSpacing[1]
 
-    numOfSlices = imgDim[2]
-    imageAppendFilter = vtk.vtkImageAppend()
-    imageAppendFilter.SetAppendAxis(2)
-    for _ in range(numOfSlices):
-      imageAppendFilter.AddInputData(drawFilter.GetOutput())
-    imageAppendFilter.Update()
+      # Compute the ending point
+      endScanlineX = self.circleCenter[0] + math.sin(angleRadians) * self.radiusStopMm / self.outputImageSpacing[0]
+      endScanlineY = self.circleCenter[1] + math.cos(angleRadians) * self.radiusStopMm / self.outputImageSpacing[1]
+    # Compute linear xy values
+    elif (self.transducerGeometry == "LINEAR"):
+      # Compute the starting point
+      startScanlineX = self.topLeftPixel[0] + scanline * self.scanlineSpacingPixels
+      startScanlineY = self.topLeftPixel[1]
 
-    # Add scanline labelmap to slicer scene
-    scanlineVolume.SetIJKToRASMatrix(ijkToRas)
-    scanlineVolume.SetRASToIJKMatrix(rasToIjk)
-    scanlineVolume.SetAndObserveImageData(imageAppendFilter.GetOutput())
+      # Compute the end point
+      endScanlineX = startScanlineX # Vertical line so same 'x' value
+      endScanlineY = self.topLeftPixel[1] + self.scanlineLengthPixels
+    else:
+      # ERROR: should not reach here
+      print("Error in scanlineEndPoints")
 
-    return True
+    # Combine XY values and return
+    startScanline = [startScanlineX, startScanlineY]
+    endScanline = [endScanlineX, endScanlineY]
+
+    return [startScanline, endScanline]
 
 class USGeometryTest(ScriptedLoadableModuleTest):
   """
